6075d141285225c109c8c96d582d0b67c06e7e72
AURELUS Wisner <131913172+wisnertechnology@users.noreply.github.com>
2023-11-19 20:57:23 -0500

    Update password-generator.py

--- password-generator.py
+++ password-generator.py
@@ -1,22 +1,26 @@
 import secrets
 
 
-senbol = ['!','@','#','$','%']
-def generate():
-    vale = list()
-    for i in range(ord('!'), ord('z')):
-        if 48 <= i <= 57:
-            vale.append(chr(i))
-        elif 64 <= i <= 90:
-            vale.append(chr(i))
-        elif 97 <= i <= 122:
-            vale.append(chr(i))
-        elif chr(i) in senbol:
-            vale.append(chr(i))
-            
-        else:
-            vale = vale
-    code_secret = ''.join(secrets.choice(vale) for _ in range(10))
-    return code_secret
-    
-print(generate())
+import secrets
+
+class CodeGenerator:
+    def __init__(self):
+        self.senbol = ['!', '@', '#', '$', '%']
+        self.vale = []
+
+    def generate(self):
+        for i in range(ord('!'), ord('z')):
+            if 48 <= i <= 57:
+                self.vale.append(chr(i))
+            elif 64 <= i <= 90:
+                self.vale.append(chr(i))
+            elif 97 <= i <= 122:
+                self.vale.append(chr(i))
+            elif chr(i) in self.senbol:
+                self.vale.append(chr(i))
+
+        code_secret = ''.join(secrets.choice(self.vale) for _ in range(10))
+        return code_secret
+
+code_gen = CodeGenerator()
+print(code_gen.generate())
